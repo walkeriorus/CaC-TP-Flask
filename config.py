9a7872a42d75f49468ab32b40e9c64effa1f48e0
Walter Perez <walkeriorus472@gmail.com>
2022-12-08 03:47:24 -0300

    Modificado el archivo de configuraciones config.py.Ahora se incluye una SECRET_KEY, necesaria para trabajar con flask-login y flask.sessions

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ class DefaultSettings(object):
     ENV = 'development'
     DEBUG = True
     MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_PASSWORD = ''
+    MYSQL_DATABASE_PASSWORD = '624531'
     MYSQL_DATABASE_USER = 'root'
     MYSQL_DATABASE_BD = 'sound'
+    MYSQL_DATABASE_PORT = 3310
+    SECRET_KEY = "d2cb713646bc4fd6718924b97752db2d6f798ec0fb01a24181afaaf6499ab043"
